3091636f6fc9e23d36910f884d15b60b2d28e739
livekeen <kevin@livekeen.com>
2015-04-25 22:34:12 -0400

    Added a quick angular app file

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,40 @@
+// angular.module('checkoutApp', [])
+//   .controller('CheckoutController', ['$scope', function($scope) {
+//     $scope.master = {};
+
+//     $scope.update = function(user) {
+//       $scope.master = angular.copy(user);
+//     };
+
+//     $scope.reset = function() {
+//       $scope.user = angular.copy($scope.master);
+//     };
+
+//     $scope.reset();
+//   }]);
+
+(function() {
+  // Create angular app
+  var app = angular.module('checkoutApp', []);
+
+  // Create controller
+  app.controller("CheckoutController", function(){
+
+    this.checkout = {};
+
+    this.submitCheckout = function(product){
+      product.checkout.push(this.checkout);
+      this.checkout = {};
+    };
+  });
+
+  var skus = [{
+    name: 'Funky Socks',
+    price: '99.99',
+    frequency: 'monthly'
+  },{
+    name: 'Weird Socks',
+    price: '99.99',
+    frequency: 'yearly'
+  }];
+})();
